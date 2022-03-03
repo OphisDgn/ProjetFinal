6be359b88fdf38a38be6c18f682473f131e564ba
Celine Lieutaud <celine.lieutaud@ynov.com>
2022-03-03 15:02:28 +0100

    init back + function api delete/add in cart

--- Back/src/Controller/APIController.php
+++ Back/src/Controller/APIController.php
@@ -6,14 +6,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use App\Service\RickAndMortyGestion;
+use App\Entity\Product;
+use Exception;
 
 #[Route('/api', name: 'api')]
 class APIController extends AbstractController
 {
     #[Route('/', name: 'api_home')]
-    public function addition(Request $request): Response
+    public function index(Request $request): Response
     {
         return $this->json(['message' => "Hello world"]);
     }
+
+    #[Route('/products', name: 'api_products', methods: ["GET"])]
+    public function products(Request $request, RickAndMortyGestion $rickAndMortyGestion): Response
+    {
+        return $this->json($rickAndMortyGestion->findAll());
+    }
+
+    #[Route('/products/{id}', name: 'api_product', methods: ["GET"])]
+    public function productById(Request $request, Product $product): Response
+    {
+        return $this->json($product);
+    }
+
+    #[Route('/cart/{id}', name: 'api_add_product', methods: ["POST"])]
+    public function addProductToCart(Request $request, RickAndMortyGestion $rickAndMortyGestion, Product $product): Response
+    {
+        try {
+            $data = json_decode($request->getContent(), true);
+            $quantity = intval($data['quantity']);
+            $rickAndMortyGestion->addProductToCart($product, $quantity);
+        }
+        catch (\Exception $e) {
+            return $this->json(["error" => "Too many sorry"]);
+        }
+    }
+
+    #[Route('/cart/{id}', name: 'api_delete_product', methods: ["DELETE"])]
+    public function deleteProductToCart(Request $request, RickAndMortyGestion $rickAndMortyGestion, Product $product)
+    {
+        $cart = $rickAndMortyGestion->deleteProductToCart($product);
+    }
+
+    
+    #[Route('/cart', name: 'api_cart', methods: ["GET"])]
+    public function cart(Request $request, RickAndMortyGestion $rickAndMortyGestion): Response
+    {
+        return $this->json($rickAndMortyGestion->findCart());
+    }
+
 }
---
+++ Back/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use App\Entity\Product;
+use App\Service\RickAndMortyApiService;
+
+class AppFixtures extends Fixture
+{
+    
+    private $prices = ["8", "9,99", "10", "15", "13,50"];
+    private $quantities = [0,2,5,20,30,70];
+    private RickAndMortyApiService $rickAndMortyApiService;
+
+    public function __construct(RickAndMortyApiService $rickAndMortyApiService)
+    {
+        $this->rickAndMortyApiService = $rickAndMortyApiService;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $data = $this->rickAndMortyApiService->loadApi();
+        foreach($data as $model) {
+            $product = new Product();
+            $product->setName($model->getName());
+            $product->setPrice($this->prices[array_rand($this->prices, 1)]);
+            $product->setQuantity($this->quantities[array_rand($this->quantities, 1)]);
+            $product->setImage($model->getImage());
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+}
